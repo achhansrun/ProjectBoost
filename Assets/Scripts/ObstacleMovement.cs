4df7309f96b11c43fc6d86937b405e41432be3b0
achhansrun <akeragon907@gmail.com>
2022-07-16 14:06:13 -0800

    Added obstacle movement script

---
+++ Assets/Scripts/ObstacleMovement.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstacleMovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        while(Time.time < 3){
+
+            transform.Translate(0, 3, 0);
+
+        }
+    }
+}
